31a8b95d186f16091df3c8d6b43eaf8894f6ec8f
Sebastian Rojas <serogr99@gmail.com>
2025-11-23 20:48:05 -0600

    Se creo el controlador MostrarNombreServlet.java para la tarea 7

--- src/main/java/org/serogr/apiservlet/webapp/cookies/controllers/MostrarNombreServlet.java
+++ src/main/java/org/serogr/apiservlet/webapp/cookies/controllers/MostrarNombreServlet.java
@@ -1,4 +1,33 @@
 package org.serogr.apiservlet.webapp.cookies.controllers;
 
-public class MostrarNombreServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet({"/nombre.html", ""})
+public class MostrarNombreServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nombre = (String) req.getAttribute("nombre");
+        resp.setContentType("text/html;charset=UTF8");
+        /*
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("   <head>");
+            out.println("       <meta charset=\"UTF-8\">");
+            out.println("           <title>Tarea 7 Listener</title>");
+            out.println("   </head>");
+            out.println("   <body>");
+            out.println("       <h1>Tarea 7 Listener</h1>");
+            out.println("       <h2> Mi nombre es " + nombre + "</h2>");
+            out.println("   </body>");
+            out.println("</html>");
+        }*/
+    }
 }
