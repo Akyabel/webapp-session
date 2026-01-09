64cd6e5bcec4e28a0db7e9b95262b7bca121411f
Sebastian Rojas <serogr99@gmail.com>
2026-01-09 17:37:17 -0600

    Se agrega la interfaz Repository.java para manejar los métodos de ProductoRepositoryJDBCImpl.java

--- src/main/java/org/serogr/apiservlet/webapp/cookies/repositories/Repository.java
+++ src/main/java/org/serogr/apiservlet/webapp/cookies/repositories/Repository.java
@@ -1,4 +1,15 @@
 package org.serogr.apiservlet.webapp.cookies.repositories;
 
-public interface Repository {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Repository<T> {
+    //Lista los objetos (El método es de tipo genérico)
+    List<T> listar() throws SQLException;
+    // Obtiene el objeto por ID (El método es de tipo genérico)
+    T porID(Long id) throws SQLException;
+    //Guardar (El método es de tipo genérico)
+    void guardar(T t) throws SQLException;
+    //Eliminar (El método es de tipo genérico)
+    void eliminar (Long id) throws SQLException;
 }
