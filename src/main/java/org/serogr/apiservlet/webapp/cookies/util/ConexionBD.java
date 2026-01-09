621351185f74938432b9654f500b977e2088b03e
Sebastian Rojas <serogr99@gmail.com>
2026-01-09 17:38:24 -0600

    Se crea la clase ConexionBD.java para inicializar la conexión a la BD

--- src/main/java/org/serogr/apiservlet/webapp/cookies/util/ConexionBD.java
+++ src/main/java/org/serogr/apiservlet/webapp/cookies/util/ConexionBD.java
@@ -1,4 +1,15 @@
 package org.serogr.apiservlet.webapp.cookies.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+    private static String url = "jdbc:mysql://localhost:3306/productos_prueba?serverTimeZone=America/Mexico_City";
+    private static String username = "root";
+    private static String password = "5804220";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
 }
