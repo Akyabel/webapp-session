bec96e62e956e1c9d9bd9351254a256a9ed58f94
Sebastian Rojas <serogr99@gmail.com>
2026-01-09 18:18:53 -0600

    Se creo la excepción ServiceJDBCException.java para manejar la excepción en la conexión a la BD

--- src/main/java/org/serogr/apiservlet/webapp/cookies/services/ServiceJDBCException.java
+++ src/main/java/org/serogr/apiservlet/webapp/cookies/services/ServiceJDBCException.java
@@ -1,4 +1,11 @@
 package org.serogr.apiservlet.webapp.cookies.services;
 
-public class ServiceJDBCException {
+public class ServiceJDBCException extends RuntimeException{
+    public ServiceJDBCException(String mensaje){
+        super(mensaje);
+    }
+
+    public ServiceJDBCException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
